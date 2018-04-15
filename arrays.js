12a0923b3f27f68e0c1c8f7eca3e10438efb2193
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-15 17:37:21 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,39 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles',
+  ]
+
+
+function addElementToBeginningOfArray(chocolateBars, c) {
+return [c, ... chocolateBars];}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, c) {
+  return [chocolateBars.unshift(c)];}
+
+function addElementToEndOfArray(chocolateBars, c) {
+  return [...chocolateBars, c];}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, c) { return [chocolateBars.push(c)];}
+
+function accessElementInArray(chocolateBars, c) {
+  return chocolateBars[c];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
+  return chocolateBars.shift();
+}
+
+function removeElementFromBeginningOfArray(chocolateBars){
+  return [chocolateBars.slice(1)];
+}
+
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+  return [chocolateBars.pop()];
+}
+
+function removeElementFromEndOfArray(chocolateBars){
+  return [chocolateBars.slice(0, chocolateBars.length-1)];
+}
+
